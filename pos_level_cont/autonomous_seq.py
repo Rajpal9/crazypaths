7d10ccef59affbbc45e2c128de701823c18f6229
Shreeram Murali <shreeramm@iisc.ac.in>
2022-11-10 19:55:14 +0530

    qtm to extpos

--- pos_level_cont/autonomous_seq.py
+++ pos_level_cont/autonomous_seq.py
@@ -8,12 +8,19 @@ from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.utils import uri_helper
 from cflib.positioning.position_hl_commander import PositionHlCommander
 import numpy as np
+import math
+
+from mocaptools import sqrt, Pose, QtmWrapper
+
 # URI to the Crazyflie to connect to
 uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E701')
 
+# argparse this or replace with relative path  
 path_np = np.array(np.genfromtxt('/home/rajpal/CF programs/crazypaths/paths/circle_waypoints.csv',delimiter=',', skip_header=1, dtype=float).tolist())
-
 sequence = path_np[:,1:]
+
+QTM_IP = '192.168.0.106' 
+CF_BODY = 'cf1'
 # Change the sequence according to your setup
 #             x    y    z  YAW
 # sequence = [
@@ -72,6 +79,10 @@ def wait_for_position_estimator(scf):
 
 def reset_estimator(scf):
     cf = scf.cf
+    # activate Kalman estimator 
+    cf.param.set_value('stabilizer.estimator', '2')
+
+    # reset estimator
     cf.param.set_value('kalman.resetEstimation', '1')
     time.sleep(0.1)
     cf.param.set_value('kalman.resetEstimation', '0')
@@ -114,11 +125,29 @@ def run_sequence(scf, sequence):
     time.sleep(0.1)
 
 
+def send_extpose_rot_matrix(cf, x, y, z, rot):
+    """Send full pose from mocap to Crazyflie."""
+    # get quaternion from rot matrix
+    qw = sqrt(1 + rot[0][0] + rot[1][1] + rot[2][2]) / 2
+    qx = sqrt(1 + rot[0][0] - rot[1][1] - rot[2][2]) / 2
+    qy = sqrt(1 - rot[0][0] + rot[1][1] - rot[2][2]) / 2
+    qz = sqrt(1 - rot[0][0] - rot[1][1] + rot[2][2]) / 2
+    # Normalize the quaternion
+    ql = math.sqrt(qx ** 2 + qy ** 2 + qz ** 2 + qw ** 2)
+    # Send to Crazyflie
+    cf.extpos.send_extpose(x, y, z, qx / ql, qy / ql, qz / ql, qw / ql)
+
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
 
+    mocap = QtmWrapper()
+    
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        reset_estimator(scf) 
+
+        mocap.on_cf_pose = lambda pose: send_extpose_rot_matrix(scf.cf, pose[0], pose[1], pose[2], pose[3])
+
+        reset_estimator(scf)
         # start_position_printing(scf)
         run_sequence(scf, sequence)
+        mocap.close()
